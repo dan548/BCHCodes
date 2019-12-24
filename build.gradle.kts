c85ab49678bae5894864225cff002ce56f6ef779
dan548 <danfromomsk@gmail.com>
2019-12-24 13:10:53 +0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "ru.omsu.imit"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.github.ajalt:clikt:2.3.0")
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
